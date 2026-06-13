c167447bf97ac676322f17876fe455723215bb44
elberthprofissional <elberthprofissional@gmail.com>
2026-06-12 22:02:23 -0300

    FORCE REBUILD v1.1.0: Full UI Refactor

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+// Cache bust: v1.1.0 - Full UI Refactor
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
