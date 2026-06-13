1e9cc7b0b56b273325c725f4cab6f5bc6986f42b
elberthprofissional <elberthprofissional@gmail.com>
2026-06-13 19:06:29 -0300

    refactor: rename Testimonials to TestimonialsSlider to force Vercel sync

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import About from '../components/About';
 import Services from '../components/Services';
-import Testimonials from '../components/Testimonials';
+import Testimonials from '../components/TestimonialsSlider';
 import Gallery from '../components/Gallery';
 import Location from '../components/Location';
 import Footer from '../components/Footer';
