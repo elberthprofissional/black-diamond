57296257a9ea3d24914dd12dd1cdbe7188ed04fa
elberthprofissional <elberthprofissional@gmail.com>
2026-07-01 19:23:20 -0300

    Remove SQL files desnecessários - mantém apenas estrutura_black_diamond.sql

--- limpar_cancelamentos.sql
+++
@@ -1,5 +0,0 @@
--- Remove todos os agendamentos cancelados (testes)
-DELETE FROM bookings WHERE status = 'cancelled';
-
--- Remove clientes bloqueados de teste
-DELETE FROM clients WHERE name = 'BLOQUEADO' AND phone = '00000000000';
